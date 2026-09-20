78f6f8505e31b74cd54dbb733c592a5e34e948ba
urmasv <55360524+urmasv@users.noreply.github.com>
2026-09-20 13:15:13 +0300

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_columnconnector';
-$plugin->version   = 2026062800;
+$plugin->version   = 2026062900;
 $plugin->requires  = 2022112800; // Moodle 4.1.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1.1';
+$plugin->release   = '1.1.2';
